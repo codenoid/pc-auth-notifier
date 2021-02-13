b874af3368debe25dd1a6b238d7aeb59c43f8b11
Rubi <14269809+codenoid@users.noreply.github.com>
2021-02-14 01:14:25 +0700

    add Status field

--- shared-packages/model/log.go
+++ shared-packages/model/log.go
@@ -5,6 +5,7 @@ type AuthLog struct {
 	LogID     string `json:"log_id"`
 	Host      string `json:"host"`
 	Username  string `json:"username"`
+	Status    string `json:"status"`
 	Timestamp int64  `json:"ts"`
 	Raw       string `json:"raw"`
 	MachineID string `json:"machine_id"`
