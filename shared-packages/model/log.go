b9749e3653233d7c3321cdd2bac47ee06a999813
Rubi <14269809+codenoid@users.noreply.github.com>
2021-02-14 01:04:17 +0700

    add shared-packages

---
+++ shared-packages/model/log.go
@@ -0,0 +1,11 @@
+package model
+
+// AuthLog auth log model
+type AuthLog struct {
+	LogID     string `json:"log_id"`
+	Host      string `json:"host"`
+	Username  string `json:"username"`
+	Timestamp int64  `json:"ts"`
+	Raw       string `json:"raw"`
+	MachineID string `json:"machine_id"`
+}
